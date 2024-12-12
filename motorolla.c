6af8cb08de8fee4485c7741d610d0e91c1aac8fb
Eugen Sokur <darriusvirt@tutamail.com>
2024-12-13 01:17:36 +0300

    first commit - о да

---
+++ motorolla.c
@@ -0,0 +1,12 @@
+#include  <locale.h>
+#include <stdio.h>
+int main (void) {
+int x = 8, y = 9;
+double i = 5;
+	while (i <= 5.7) {
+	i = i + 0.2;
+	printf("%d", x);
+ 		 if(i % 5.2 == 0) { printf("%d", y); }
+	}
+return 0;
+}
